ab605b44d3c4bac211fe07ff29a270dfee35f5ef
Joram Okwaro <joramokwaro@gmail.com>
2011-10-31 22:28:43 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Convivial::Application.config.secret_token = 'b8a7e6b26ec6ad18e1621bee7cdbed9b49192340bd980bbb7f78b1abccf7f379dc2c3af53e1c3cc4fb16196b6d47265db2afd833fc5fa63ca0a95ea6ba00bcfa'
